1a3e0774749cb8992c068bebbd736ba9c9568a33
OlegBrest <pop457045@mail.ru>
2018-01-15 06:46:16 +0300

    need to save gridview in main activity

--- app/src/main/java/com/example/pop/myeconomizer/MainActivity.java
+++ app/src/main/java/com/example/pop/myeconomizer/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,9 +28,9 @@ class goods_type
         this.Volume = 0.0;
     }
 
-    public String getName() {return Name;}
-    public double getCost() {return Cost;}
-    public double getVolume() {return Volume;}
+    public String getName() {return this.Name;}
+    public double getCost() {return this.Cost;}
+    public double getVolume() {return this.Volume;}
 
     public void setName (String name) {this.Name =name;}
     public void setCost (double cost) {this.Cost =cost;}
@@ -47,22 +48,49 @@ private ArrayAdapter<goods_type> adapter;
 private ArrayList<goods_type> goods;
 private ArrayList<goods_type> listOfGoods;
 
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putDouble("myDouble", 1.9);
+        savedInstanceState.putInt("MyInt", 1);
+        savedInstanceState.putString("MyString", "Welcome back to Android");
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d("MainActiv","onSave");
+    }
+
+        @Override
+        public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+            // This bundle has also been passed to onCreate.
+            boolean myBoolean = savedInstanceState.getBoolean("MyBoolean");
+            double myDouble = savedInstanceState.getDouble("myDouble");
+            int myInt = savedInstanceState.getInt("MyInt");
+            String myString = savedInstanceState.getString("MyString");
+            Log.d("MainActiv","onRestore");
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        newbutton = (Button) findViewById(R.id.new_bttn);
-        listOfGoods = new ArrayList<>();
-        gv = (GridView) findViewById(R.id.Goods_GridView);
-        adapter = new GoodsListAdapter(this, R.layout.list_items,listOfGoods);
-        gv.setAdapter(adapter);
-        newbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(getApplicationContext(),SecondActivity.class);
-                startActivityForResult(intent,1);
-            }
-        });
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            newbutton = (Button) findViewById(R.id.new_bttn);
+            listOfGoods = new ArrayList<>();
+            gv = (GridView) findViewById(R.id.Goods_GridView);
+            adapter = new GoodsListAdapter(this, R.layout.list_items, listOfGoods);
+            gv.setAdapter(adapter);
+            newbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                    startActivityForResult(intent, 1);
+                }
+            });
+        Log.d("MainActiv","onCreate");
     }
     @Override
     protected void onActivityResult (int requestCode, int resultCode , Intent data)
@@ -75,14 +103,6 @@ private ArrayList<goods_type> listOfGoods;
 
         listOfGoods.add (gt);
         adapter.notifyDataSetChanged();
-
-        int count = this.gv.getChildCount();
-        for (int i = 0; i < count; i++)
-        {
-            View cell = this.gv.getChildAt(i);
-            TextView cellText = (TextView) cell.findViewById(R.id.txt_name);
-            cellText.setTextColor(0);
-        }
-
+        Log.d("MainActiv","onActResult");
     }
 }
